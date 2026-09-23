5dcb5daaf7cfb7e59b999fbde2e0b3c22f15d426
Dmytro Kazarov <d.kazarov1361@gmail.com>
2026-09-23 19:41:55 +0300

    feat: add menu nav markup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import './styles/main.css';
 
-import { createIcons, ChevronRight, ShoppingCart } from 'lucide';
+import { createIcons, ChevronRight, ShoppingCart, House, Wallet, CirclePlus, ChartNoAxesCombined, Settings } from 'lucide';
 
 createIcons({
   icons: {
     ChevronRight,
     ShoppingCart,
+    House,
+    Wallet,
+    CirclePlus,
+    ChartNoAxesCombined,
+    Settings,
   },
 });
